e1722595ff05c371e913e1b1f90baa75c67d6137
NurIkhwanAnnafis <ikhwan@twiscoder.com>
2020-10-04 01:47:15 +0700

    delete unused variable

--- Beginner/05. Javascript/equality_in_array.js
+++ Beginner/05. Javascript/equality_in_array.js
@@ -12,7 +12,7 @@ process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
+process.stdin.on('end' => {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
@@ -46,8 +46,6 @@ function equalizeArray(arr) {
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const n = parseInt(readLine(), 10);
 
     const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
 
