a356f1d3fd1fe9cabc2ce55170887869b76f0701
ashish-thorat <ashish.t.dev@gmail.com>
2020-10-06 22:59:30 +0530

    C# code to convert 1st character of a string to upper case

---
+++ Beginner/10. C#/FirstCharUpperCase/FirstCharUpperCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstCharUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "its a bright sunny day.";
+            Program p = new Program();
+            Console.WriteLine("Sentence before formatting is - {0}",sentence);
+            sentence = p.UpperCase(sentence);
+            Console.WriteLine("Sentence with 1st character as upper case is - {0}", sentence);
+            Console.ReadKey();
+        }
+
+        public string UpperCase(string sentence)
+        {
+            return char.ToUpper(sentence[0]) + sentence.Substring(1);
+
+        }
+    }
+}
