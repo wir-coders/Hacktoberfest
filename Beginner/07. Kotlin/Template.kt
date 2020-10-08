032062fd4b51d91a240e8db189ef161fdc1cf6a3
fida501 <vivalafida123@gmail.com>
2020-10-08 09:41:06 +0700

    Editing Template.kt
    Template.kt Edited

--- Beginner/07. Kotlin/Template.kt
+++ Beginner/07. Kotlin/Template.kt
@@ -1,3 +1,15 @@
-fun main(args: Array<String>) {
-	println("Hello World")
+import java.util.Arrays
+
+open class	Test123{
+	fun plebs(){
+		print("Chat Spam, So bad LUL @AdmiralBulldog")
+	}
+}
+class ABC : Test123(){
+
+}
+
+fun  main(args: Array<String>) {
+	var a = ABC()
+	a.plebs()
 }
