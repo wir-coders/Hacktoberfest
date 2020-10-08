754ded83aed801dadbfc0fa547fa27cbf4927f3c
Agnibho Hom Chowdhury <contact.agnibho@gmail.com>
2020-10-08 11:58:59 +0530

    Create BeautifulDaysAtTheMovies.java
    added BeautifulDaysAtTheMovies solution in java

---
+++ Beginner/06. Java/BeautifulDaysAtTheMovies.java
@@ -0,0 +1,26 @@
+public class BeautifulDaysAtTheMovies {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		int k = sc.nextInt();
+		int count = 0;
+		for (int m = i; m <= j; m++) {
+			int reversedString = getReversed(m);
+			if ((reversedString - m) % k == 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
+
+	private static int getReversed(int i) {
+		String str = i + "";
+		String newValue = "";
+		for (int k = str.length() - 1; k >= 0; k--) {
+			newValue = newValue + str.charAt(k);
+		}
+		return Integer.parseInt(newValue);
+	}
+}
