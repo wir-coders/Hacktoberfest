af407aa7b1bba31e3ee85b9ffa83eae891d21538
Siva Kumar G <61716135+SivaKumar2001@users.noreply.github.com>
2020-10-11 23:42:05 +0530

    Hello World.cpp

---
+++ Beginner/04. C++/Hello World.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // Header file
+
+using namespace std;
+
+int main() // main method
+{
+    cout << "Hello,World!" << endl;// prints Hello,World
+    cout << "Hello,World! \n"; //It also prints Hello,World
+    return 0; // successful termination
+
+}
