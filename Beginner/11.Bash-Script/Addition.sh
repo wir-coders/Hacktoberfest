343cdd6d1dc4ac25c2a67e8f86de447a508df644
Sourabh <ssourabh950@gmail.com>
2020-10-21 13:51:44 +0530

    Adding new Directory for Bash programming language

---
+++ Beginner/11.Bash-Script/Addition.sh
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash
+
+echo -n 'Enter the First Number: '
+read -r a
+echo -n 'Enter the Second Number: '
+read -r b
+echo "$a + $b = $((a+b))"
---
+++ Beginner/11.Bash-Script/Hello .sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Avoid spaces around the assignment operator (=)
+var="Hello World"
+ 
+# print it 
+echo "$var"
+ 
+# Another way of printing it
+printf "%s\n" "$var"
