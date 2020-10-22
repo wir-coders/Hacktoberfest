be4ea133890c1514f0b1d893dfc8ab6f231a689e
Moksha <mokshasood06@gmail.com>
2020-10-22 10:16:33 +0530

    Added bubble sort

---
+++ Beginner/03. Python/bubble_sort.py
@@ -0,0 +1,13 @@
+#Bubble sort
+def bubbleSort(arr): 
+    n = len(arr) 
+    for i in range(n-1): 
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+  
+arr = [74, 36, 15, 92, 10, 47, 82] 
+bubbleSort(arr) 
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+    print (arr[i],end=" ")
