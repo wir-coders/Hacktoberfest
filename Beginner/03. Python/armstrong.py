c5777e79a4028bfd39574376bee649939cfd33d2
MANSI KANHEY <47391058+mansikanhey@users.noreply.github.com>
2020-10-30 16:11:10 +0530

    Update armstrong.py

--- Beginner/03. Python/armstrong.py
+++ Beginner/03. Python/armstrong.py
@@ -5,13 +5,13 @@
 num = int(input("Enter a number: "))
 
 # initialize sum
-sum = 0
+total = 0
 
 # find the sum of the cube of each digit
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
+   total += digit ** 3
    temp //= 10
 
 # display the result
