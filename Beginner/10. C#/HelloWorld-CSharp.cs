c9e055edcd6a8a4fac80a14d148a6bc7b7c4e409
ashish-thorat <ashish.t.dev@gmail.com>
2020-10-31 12:06:23 +0530

    Basic Hello Workld program in C#

---
+++ Beginner/10. C#/HelloWorld-CSharp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
