243e921be5c5d41d2770dc00dcf5f2a3d4cbb514
Vikashyadav-8090 <73946352+Vikashyadav-8090@users.noreply.github.com>
2020-11-27 00:57:31 +0530

    Update Even_or_Odd.py
    updated this to some little advance method which all beginners should know .

--- Beginner/03. Python/Even_or_Odd.py
+++ Beginner/03. Python/Even_or_Odd.py
@@ -1,5 +1,8 @@
-x=int(input("Enter a number to identify Even or Odd"))
-if x%2==0:
-    print("The number you entered is Even")
-else:
-    print("The number you entered is Odd")
+  def even_no(number):
+#         this is one more method to print the even -odd number little advance method in begginers section .
+        
+        if number%2==0:# checking cond. using if ladder
+            print(" the number u entered is Even ")
+        else:
+            print(" the number u entered is Odd ")
+            
